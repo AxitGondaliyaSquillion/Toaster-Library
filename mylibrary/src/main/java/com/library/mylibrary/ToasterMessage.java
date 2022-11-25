2dad68f22ae7247d9aa66e022d7ff3c91bc43aa7
Axit Gondaliya <axit@squillion.tech>
2022-11-25 15:30:25 +0530

    Update ToasterMessage.java

--- mylibrary/src/main/java/com/library/mylibrary/ToasterMessage.java
+++ mylibrary/src/main/java/com/library/mylibrary/ToasterMessage.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public static void s(Context c,String message){
-        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    public static void showToast(Context context,String message){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
 }
